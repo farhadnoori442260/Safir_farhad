2bd1e5ebb65f9e3134745ea6bba01b040ef8a997
farhadnoori442260 <farhadnoori552@gmail.com>
2026-06-25 12:50:26 +0330

    Initialize Flutter app with home screen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const SafirApp());
+}
+
+class SafirApp extends StatelessWidget {
+  const SafirApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: Locale('fa', 'AF'), 
+      home: SafirHomeScreen(),
+    );
+  }
+}
